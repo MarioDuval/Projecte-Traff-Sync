a52ddef54b43ca5153b188857270e60b69554e52
MarioDuval <77450981+MarioDuval@users.noreply.github.com>
2022-02-20 14:41:45 +0100

    main

--- proyect/js/script.js
+++ proyect/js/script.js
@@ -3,3 +3,22 @@ function myFunction(x) {
     
     document.getElementById('dropdown').style.display = document.getElementById('dropdown').style.display =='block' ?   'none' : 'block';
 }
+
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
